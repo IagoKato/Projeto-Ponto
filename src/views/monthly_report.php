81197149ad2f8155a73a685b4f4a9b379a1dfb82
IagoKato <94723919+IagoKato@users.noreply.github.com>
2023-08-02 17:10:48 -0300

    Update monthly_report.php

--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -1,6 +1,6 @@
 <main class="content">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<?php
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <?php
 		renderTitle(
 			'Relatório Mensal',
 			'Acompanhe seu saldo de horas',
@@ -44,24 +44,33 @@
 				<th>Saída</th>
 				<th>Saldo</th>
 			</thead>
-			<tbody>
-				<?php foreach($report as $registry): ?>
-					<tr>
-						<td><?= formatDateWithLocale($registry->work_date, '%A, %d de %B de %Y') ?></td>
-						<td><?= $registry->time1 ?></td>
-						<td><?= $registry->time2 ?></td>
-						<td><?= $registry->time3 ?></td>
-						<td><?= $registry->time4 ?></td>
-						<td><?= $registry->getBalance() ?></td>
-					</tr>
-				<?php endforeach ?>
-				<tr class="bg-primary text-white">
-					<td>Horas Trabalhadas</td>
-					<td colspan="3"><?= $sumOfWorkedTime ?></td>
-					<td>Saldo Mensal</td>
-					<td><?= $balance ?></td>
-				</tr>
-			</tbody>	
-		</table>
+            <tbody>
+            <?php foreach($report as $registry): ?>
+                <?php
+                // Obter o dia da semana (0 - domingo, 6 - sábado)
+                $dayOfWeek = date('w', strtotime($registry->work_date));
+
+                // Verificar se o dia da semana não é sábado (6) nem domingo (0)
+                if ($dayOfWeek !== '0' && $dayOfWeek !== '6'):
+                    ?>
+                    <tr>
+                        <td><?= formatDateWithLocale($registry->work_date, '%A, %d de %B de %Y') ?></td>
+                        <td><?= $registry->time1 ?></td>
+                        <td><?= $registry->time2 ?></td>
+                        <td><?= $registry->time3 ?></td>
+                        <td><?= $registry->time4 ?></td>
+                        <td><?= $registry->getBalance() ?></td>
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach ?>
+            <tr class="bg-primary text-white">
+                <td>Horas Trabalhadas</td>
+                <td colspan="3"><?= $sumOfWorkedTime ?></td>
+                <td>Saldo Mensal</td>
+                <td><?= $balance ?></td>
+            </tr>
+            </tbody>
+
+        </table>
 	</div>
 </main>
